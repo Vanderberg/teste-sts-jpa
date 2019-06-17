8b1a3d5a4efa5217caaa695a9e7983b06c446345
VanderNunesBR <tec.vander.nunes@gmail.com>
2019-06-16 23:41:43 -0300

    Adicionado projeto

---
+++ src/main/java/com/vandernunes/api/TesteStsJpaTravisApplication.java
@@ -0,0 +1,13 @@
+package com.vandernunes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteStsJpaTravisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteStsJpaTravisApplication.class, args);
+	}
+
+}

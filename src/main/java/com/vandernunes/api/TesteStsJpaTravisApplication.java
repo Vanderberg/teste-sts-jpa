c6ea68414c7c6b77f686902766c46459d5d0a2aa
VanderNunesBR <tec.vander.nunes@gmail.com>
2019-07-17 23:07:11 -0300

    Ajustado Build

--- src/main/java/com/vandernunes/api/TesteStsJpaTravisApplication.java
+++ src/main/java/com/vandernunes/api/TesteStsJpaTravisApplication.java
@@ -1,13 +1,51 @@
 package com.vandernunes.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.vandernunes.api.entities.Empresa;
+import com.vandernunes.api.repositories.EmpresaRepository;
 
 @SpringBootApplication
 public class TesteStsJpaTravisApplication {
 
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TesteStsJpaTravisApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Vander IT");
+			empresa.setCnpj("74645215000104");
+			
+			this.empresaRepository.save(empresa);
+
+			List<Empresa> empresas = empresaRepository.findAll();
+			empresas.forEach(System.out::println);
+			
+			//Empresa empresaDB = empresaRepository.findById();
+			//System.out.println("Empresa por ID: " + empresaDB);
+			
+//			empresaDB.setRazaoSocial("Kazale IT Web");
+//			this.empresaRepository.saveAll(empresaDB);
+
+			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
+			System.out.println("Empresa por CNPJ: " + empresaCnpj);
+			
+			this.empresaRepository.deleteById(1L);
+			empresas = empresaRepository.findAll();
+			System.out.println("Empresas: " + empresas.size());
+			
+		};
+	}
 }
--- src/main/java/com/vandernunes/api/repositories/EmpresaRepository.java
+++ src/main/java/com/vandernunes/api/repositories/EmpresaRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vandernunes.api.entities.Empresa;
 
-public interface EmpresaRepositories extends JpaRepository<Empresa, Long> {
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 	
 	Empresa findByCnpj(String cnpj);
 
